a0a519701181b065dda8cf6440ce36ddc18f3c75
Lyubov Alekseyeva <lyubov.shestopalova@gmail.com>
2018-04-02 15:37:54 +0300

    Функция по clickHandler перенесена в файл building.js

--- building.js
+++ building.js
@@ -1,6 +1,18 @@
 
 let welcome = require('./welcome');
-welcome('Алексеева Любовь', 'fullName', 'click');
+welcome(name = 'Алексеева Любовь', className = 'fullName', function(event) {
+  const p = document.querySelector('p');
+  const moment =  require('moment');
+  moment.locale('ru');
+  const date = moment().format('LL');
+  if (!p.className){
+    p.className = className;
+    p.textContent = `${name} ${date}`;
+  } else {
+    p.classList.remove(className);
+    p.textContent = name;
+  }
+});
 
 module.exports = {
   welcome
--- welcome.js
+++ welcome.js
@@ -1,18 +1,7 @@
 module.exports = function(fullName, className, clickHandler) {
-  let p = document.createElement('p');
+  const p = document.createElement('p');
   p.textContent = fullName;
   document.body.appendChild(p);
   clicker = document.querySelector('p');
-  clicker.addEventListener(clickHandler, function(event) {
-    if (!p.className){
-      p.className = className;
-      let moment =  require('moment');
-      moment.locale('ru');
-      let date = moment().format('LL');
-      p.textContent = `${fullName} ${date}`;
-    } else {
-      p.classList.remove(className);
-      p.textContent = fullName;
-    }
-  });
+  clicker.addEventListener('click', clickHandler);
 }
